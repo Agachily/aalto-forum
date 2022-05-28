17892debd7a8e56154acd049ac471cd5e8e8ad29
Agachily <zetongzhao@foxmail.com>
2022-05-28 14:52:49 +0300

    Integrate Elasticsearch to project

--- src/main/java/com/aalto/myBBS/service/entity/DiscussPost.java
+++ src/main/java/com/aalto/myBBS/service/entity/DiscussPost.java
@@ -1,16 +1,34 @@
 package com.aalto.myBBS.service.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 import java.util.Date;
 
+/**
+ * 指定索引，分片和副本，用于向Elastic search中存储
+ */
+@Document(indexName = "discusspost", shards = 3, replicas = 2)
 public class DiscussPost {
+    @Id
     private int id;
+    @Field(type = FieldType.Integer)
     private int userId;
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart") // analyzer, searchAnalyzer是解析器
     private String title;
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String content;
+    @Field(type = FieldType.Integer)
     private int type;
+    @Field(type = FieldType.Integer)
     private int status;
+    @Field(type = FieldType.Date)
     private Date createTime;
+    @Field(type = FieldType.Integer)
     private int commentCount;
+    @Field(type = FieldType.Double)
     private double score;
 
     public int getId() {
